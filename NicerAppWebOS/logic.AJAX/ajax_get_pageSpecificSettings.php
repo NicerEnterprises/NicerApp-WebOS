780da09f8cb119795df8339c64fcc1d55b16fb7c
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2024-01-27 18:01:29 +0100

    Rewrite of license. fucking github doesnt even allow easy deletion of repositories anymore.

--- NicerAppWebOS/logic.AJAX/ajax_get_pageSpecificSettings.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$rootPathNA = realpath(dirname(__FILE__).'/../..').'/NicerAppWebOS';
-require_once ($rootPathNA.'/boot.php');
-//echo '<!--'; var_dump ($_GET); echo '-->';
-    global $naWebOS;
-    $r = $naWebOS->getPageCSS(true, true, $_GET['includeClientOnlyThemes']==='true'?true:false, $_GET['stickToCurrentSpecificity']==='true'?true:false, $_GET['specificityName']);
-    if ($r && $r!=='') $r = $naWebOS->getPageCSS(true, true, $_GET['includeClientOnlyThemes']==='true'?true:false, $_GET['stickToCurrentSpecificity']==='true'?true:false);
-    echo $r;
-?>
--- initialUpload.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add -v *
-git commit -m "N-th commit of the NicerApp WCS (Website Control System) git repository. See https://facebook.com/rene.veerman.90 for more details."
-git branch -M main
-git remote add origin https://github.com/NicerAppWebOS/nicerapp.git
-git push -u origin main
