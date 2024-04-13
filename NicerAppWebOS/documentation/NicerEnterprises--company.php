c71a161468f8cc9f7601ab1ab17f04eb32277bf3
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2024-04-13 05:46:38 +0200

    STABLE - for most apps - v5.y.z

---
+++ NicerAppWebOS/documentation/NicerEnterprises--company.php
@@ -0,0 +1,21 @@
+<?php
+    global $naWebOS;
+    require_once ($naWebOS->basePath.'/NicerAppWebOS/domainConfigs/'.$naWebOS->domain.'/pageHeader.php');
+?>
+    <style>
+        p {
+            display : block;
+        }
+    </style>
+<h1 class="contentSectionTitle2"><span class="contentSectionTitle2_span">Nicer Enterprises - company overview</span></h1><br/><br/><br/>
+
+
+<div>
+    <p>
+    <a href="https://nicer.app" target="naHP">https://nicer.app</a>, <a href="https://said.by" target="sbHP">https://said.by</a>, <a href="https://zoned.at" target="zAt">https://zoned.at</a>, are all
+    Copyrighted 2002-2024 and owned by Nicer Enterprises company.<br/>
+    </p>
+
+
+</div>
+
