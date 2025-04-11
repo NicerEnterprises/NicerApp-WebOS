f6fb7e5a12775f77ca2e97a02fad9796a03d92d7
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2025-04-11 06:57:05 +0200

    Backup scripts now taking shape

--- NicerAppWebOS/scripts.maintenance/bap.sh
+++ NicerAppWebOS/scripts.maintenance/bap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # bap.sh = backup and publish.sh
-DATE=$(date +'%Y-%m-%d %H:%M:%S')
+DATE=$(date +'%Y-%m-%d_%H:%M:%S')
 set -x # echo on
 source ./do_upgrade_globals_manufacturer.sh
 
@@ -10,7 +10,7 @@ declare -a targetDomains=("nicer.app" "said.by")
 for TD in "${targetDomains[@]}"; do
 	echo "--- NOW WORKING ON SITE $TD"
 	mv /var/www/$TD "/var/www/$DATE $TD"
-	cp -rv /var/www/$sourceDomain /var/www/$TD
+	#cp -rv /var/www/$sourceDomain /var/www/$TD
 	cp "/var/www/$DATE $TD/NicerAppWebOS/scripts.maintenance/setPermissions.sh" /var/www/$TD/NicerAppWebOS/scripts.maintenance
 	mv /var/www/$TD/NicerAppWebOS/domainConfigs/$sourceDomain /var/www/$TD/NicerAppWebOS/domainConfigs/$TD
 	cp "/var/www/$DATE $TD/NicerAppWebOS/domainConfigs/$TD/index.javascripts.json" /var/www/$TD/NicerAppWebOS/domainConfigs/$TD
